ea0aa1d7c6cffa4eec0f8eb1f869ecfb4399a801
lihbr <lihbr@users.noreply.github.com>
2023-01-19 17:41:04 +0100

    feat: vue amsterdam talk

---
+++ 230209-open-source-ecosystem/components/PrismicEcosystem.vue
@@ -0,0 +1,119 @@
+<template>
+	<svg
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 511.88259010501235 327.1951112817923"
+		width="511.88259010501235"
+		height="327.1951112817923"
+	>
+		<g
+			transform="translate(113.11419735912466 83.15534272100052) rotate(0 77.5 12.5)"
+		>
+			<text
+				x="0"
+				y="18"
+				font-family="Virgil, Segoe UI Emoji"
+				font-size="20px"
+				fill="currentColor"
+				text-anchor="start"
+				style="white-space: pre"
+				direction="ltr"
+			>
+				@prismicio/client
+			</text>
+		</g>
+		<v-clicks>
+			<g
+				transform="translate(40.49122020719108 168.8351890856486) rotate(0 68 12.5)"
+			>
+				<text
+					x="0"
+					y="18"
+					font-family="Virgil, Segoe UI Emoji"
+					font-size="20px"
+					fill="currentColor"
+					text-anchor="start"
+					style="white-space: pre"
+					direction="ltr"
+				>
+					@prismicio/vue
+				</text>
+			</g>
+			<g
+				transform="translate(89.70412054144879 250.35114240333485) rotate(0 74.5 12.5)"
+			>
+				<text
+					x="0"
+					y="18"
+					font-family="Virgil, Segoe UI Emoji"
+					font-size="20px"
+					fill="currentColor"
+					text-anchor="start"
+					style="white-space: pre"
+					direction="ltr"
+				>
+					@nuxtjs/prismic
+				</text>
+			</g>
+			<g
+				transform="translate(323.1044996989268 126.33541855249598) rotate(0 79 12.5)"
+			>
+				<text
+					x="0"
+					y="18"
+					font-family="Virgil, Segoe UI Emoji"
+					font-size="20px"
+					fill="currentColor"
+					text-anchor="start"
+					style="white-space: pre"
+					direction="ltr"
+				>
+					@prismicio/react
+				</text>
+			</g>
+			<g
+				transform="translate(307.9241944005836 210.63824206907753) rotate(0 73 12.5)"
+			>
+				<text
+					x="0"
+					y="18"
+					font-family="Virgil, Segoe UI Emoji"
+					font-size="20px"
+					fill="currentColor"
+					text-anchor="start"
+					style="white-space: pre"
+					direction="ltr"
+				>
+					@prismicio/next
+				</text>
+			</g>
+			<g>
+				<g
+					stroke-linecap="round"
+					transform="translate(10 44.081831790057265) rotate(0 245.94129505250618 136.55663974586753)"
+				>
+					<path
+						d="M32 0 M32 0 C164.7 -0.77, 299.21 -2.63, 459.88 0 M32 0 C161.47 -0.89, 291.01 -0.59, 459.88 0 M459.88 0 C482.37 1.46, 490.88 12.33, 491.88 32 M459.88 0 C481.14 -0.48, 490.77 12.04, 491.88 32 M491.88 32 C490.01 87.57, 489.74 145.64, 491.88 241.11 M491.88 32 C491.1 87.42, 490.5 143.55, 491.88 241.11 M491.88 241.11 C491.17 262.18, 480.84 274.92, 459.88 273.11 M491.88 241.11 C490.41 264.69, 480.06 270.87, 459.88 273.11 M459.88 273.11 C323.29 274.07, 187.2 273.24, 32 273.11 M459.88 273.11 C357.33 271.55, 255.03 271.62, 32 273.11 M32 273.11 C9.83 274.87, 0.98 264.16, 0 241.11 M32 273.11 C11.06 273.95, -0.58 262.57, 0 241.11 M0 241.11 C-1.21 179.94, 0.17 120.55, 0 32 M0 241.11 C-0.24 157.4, 0.83 74.01, 0 32 M0 32 C-0.76 11.06, 9.91 1.79, 32 0 M0 32 C0.21 8.8, 11.4 -1.87, 32 0"
+						stroke="#364fc7"
+						stroke-width="1"
+						fill="none"
+					></path>
+				</g>
+				<g transform="translate(127.04854835360072 10) rotate(0 138.5 12.5)">
+					<text
+						x="138.5"
+						y="18"
+						font-family="Virgil, Segoe UI Emoji"
+						font-size="20px"
+						fill="#364fc7"
+						text-anchor="middle"
+						style="white-space: pre"
+						direction="ltr"
+					>
+						Prismic's package ecosystem
+					</text>
+				</g>
+			</g>
+		</v-clicks>
+	</svg>
+</template>
